70bf452b1d11fe33600232849a2b3e85ce15cbac
maruyu31 <you3746336@gmail.com>
2021-07-13 11:18:11 +0000

    [Add] comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
     # @last_week_books = @user.books.where(created_at: 2.week.ago.all_week(start_day = Date.beginning_of_week)).count
   end
 
+  #searchで書く場合の書き方
   # def search
   #   @user = User.find(params[:user_id])
   #   @books = @user.books
